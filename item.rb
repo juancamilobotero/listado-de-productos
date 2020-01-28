0c082f8a6b4fa85490b556c6a3414fa684770623
juancamilobotero <59903567+juancamilobotero@users.noreply.github.com>
2020-01-28 15:28:19 -0500

    Create item.rb

---
+++ item.rb
@@ -0,0 +1,18 @@
+class Item
+	attr_accessor :text
+	attr_writer :text, :checked
+
+	def initialize(text="")
+		@text = text
+		@checked = false
+		
+	end
+
+	def to_s
+		if @checked
+			"[x] " + @text
+		else
+			"[ ] " + @text
+		end
+	end
+end
